f2240d884d94b69fb8467d82cc90595e55d760c3
zaid nusair <zaid.nusair@hotmail.com>
2020-04-30 14:35:27 +0300

    added practice file to js repo

---
+++ js/add-content.js
@@ -0,0 +1,16 @@
+'use strict'
+var today = new Date(); 
+var hourNow = today.getHours();
+var greeting; 
+
+if (hourNow >18) { 
+    greeting = 'Goodevening,Class!'; 
+
+} else if (hourNow >12){
+    greeting = 'Goodafternoon,Class!'; 
+} else if(hourNow >=0){ 
+    greeting = 'Goodmorning!';
+ }else { 
+    greeting = 'Somethingwentwrong!';
+ } 
+ document.write('<h3>' + greeting + '</h3>');
